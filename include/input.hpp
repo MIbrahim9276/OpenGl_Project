0395871839341d268b7c3efc4cee96633b6a8e2c
MIbrahim9276 <m.ibrahim9276@gmail.com>
2024-02-11 01:35:52 +0200

    commit

--- include/input.hpp
+++ include/input.hpp
@@ -18,3 +18,23 @@ class Input {
     
     static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods);
 };
+
+#pragma once
+
+#include <common.hpp>
+
+typedef enum {
+  UP, DOWN, LEFT, RIGHT
+} InputType;
+
+class Input {
+  public:
+    Input();
+    void init(GLFWwindow* window);
+    
+  private:
+    GLFWwindow* _window;
+    map<int, InputType> _input_map;
+
+    static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods);
+};
