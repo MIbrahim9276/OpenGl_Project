c0ae5a87370d2d11ef84adafd31adde84fb20aa0
MIbrahim9276 <m.ibrahim9276@gmail.com>
2024-02-12 17:48:02 +0200

    fixing

--- include/input.hpp
+++ include/input.hpp
@@ -6,27 +6,6 @@ typedef enum {
   UP, DOWN, LEFT, RIGHT
 } InputType;
 
-
-
-class Input {
-  public:
-    Input(GLFWwindow* window);
-
-  private:
-    GLFWwindow* _window;
-    map<int, InputType> InputMap;
-    
-    static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods);
-};
-
-#pragma once
-
-#include <common.hpp>
-
-typedef enum {
-  UP, DOWN, LEFT, RIGHT
-} InputType;
-
 class Input {
   public:
     Input();
